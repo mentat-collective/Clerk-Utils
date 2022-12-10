e863bb5b34a37b32b982baf0dd1441c79167d028
Sam Ritchie <sritchie09@gmail.com>
2022-12-10 08:52:15 -0700

    examples macro

--- src/mentat/clerk_utils.clj
+++ src/mentat/clerk_utils.clj
@@ -14,3 +14,32 @@
                          result#
                          [v/inspect result#]))))}
      {}))
+
+
+(comment
+  ;; We had something like this that we want to explore. Get both of these
+  ;; working if we have no Clerk!
+  (import '(java.io FileNotFoundException))
+
+  (defn- try-resolve [sym]
+    (try (requiring-resolve sym)
+         (catch FileNotFoundException _ nil)))
+
+  (defmacro examples [& body]
+    (when (some-> (try-resolve 'nextjournal.clerk.config/*in-clerk*)
+                  deref)
+      `(nextjournal.clerk/with-viewer
+         {:render-fn
+          '(fn [l r]
+             (v/html
+              (into [:div.flex.flex-col]
+                    (v/inspect-children r)
+                    l)))}
+         [~@body])))
+
+  ;; similar thing for the other clerk functions, with-viewers, `clerk/tex` etc.
+
+  (examples
+   ;; So this will be totally elided
+   (def goofy 10)
+   (+ 1 2)))
