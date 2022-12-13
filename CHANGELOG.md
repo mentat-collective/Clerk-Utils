df0baee6820a2fa344ba4c433616cade73251e21
Sam Ritchie <sritchie09@gmail.com>
2022-12-13 10:30:35 -0700

    notebook

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [unreleased]
+
+## 0.1.0
+
+First real release!
+
+- Added `->clerk` and `cljs` macros.
+- Published fleshed-out documentation notebook at https://clerk-utils.mentat.org
--- src/mentat/clerk_utils.clj
+++ src/mentat/clerk_utils.clj
@@ -1,45 +1,27 @@
 (ns mentat.clerk-utils
-  (:require [nextjournal.clerk :as clerk]))
+  "Utilities, TODO fill in."
+  (:import (java.io FileNotFoundException)))
+
+(defn- ns-loaded? [sym]
+  (try (nil? (resolve sym))
+       (catch FileNotFoundException _ false)))
+
+;; ->clerk
+
+(defmacro ->clerk [body]
+  (when (ns-loaded? 'nextjournal.clerk)
+    body))
 
 ;; ## Example Macro
 
 (defmacro cljs [& exprs]
-  `(clerk/with-viewer
-     {:transform-fn clerk/mark-presented
-      :render-fn '(fn [_#]
-                    (let [result# (do ~@exprs)]
-                      (v/html
-                       (if (and (vector? result#)
-                                (not (:inspect (meta result#))))
-                         result#
-                         [v/inspect result#]))))}
-     {}))
-
-
-(comment
-  ;; We had something like this that we want to explore. Get both of these
-  ;; working if we have no Clerk!
-  (import '(java.io FileNotFoundException))
-
-  (defn- try-resolve [sym]
-    (try (requiring-resolve sym)
-         (catch FileNotFoundException _ nil)))
-
-  (defmacro examples [& body]
-    (when (some-> (try-resolve 'nextjournal.clerk.config/*in-clerk*)
-                  deref)
-      `(nextjournal.clerk/with-viewer
-         {:render-fn
-          '(fn [l r]
-             (v/html
-              (into [:div.flex.flex-col]
-                    (v/inspect-children r)
-                    l)))}
-         [~@body])))
-
-  ;; similar thing for the other clerk functions, with-viewers, `clerk/tex` etc.
-
-  (examples
-   ;; So this will be totally elided
-   (def goofy 10)
-   (+ 1 2)))
+  (->clerk
+   `(nextjournal.clerk/with-viewer
+      {:transform-fn nextjournal.clerk/mark-presented
+       :render-fn '(fn [_#]
+                     (let [result# (do ~@exprs)]
+                       (v/html
+                        (if (vector? result#)
+                          result#
+                          [v/inspect result#]))))}
+      {})))
